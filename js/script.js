c64f53ce65087078c439b75b3286320e06cec8fb
LuciSkroder <Luci.skroder@gmail.com>
2025-01-27 12:41:52 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+let gameRunning = true;
+Boolean(gameRunning);
+
+let answer = Math.ceil(Math.random() * 100);
+console.log(`${answer}`)
+
+while (gameRunning === true) {
+    let guess = parseInt(prompt(`Guess a number between 1-100`));
+    if (guess === answer) {
+        alert(`You guessed the correct number! ${answer}`);
+        break;
+    }
+    else if (guess < answer) {
+        alert(`Your guess was too low!`);
+    }
+    else {
+        alert(`Your guess was too high!`);
+    }
+}
